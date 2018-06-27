a2b35df1a608f46749bc755a89f03f75428ea656
Tiffany <tiffanyjdow@gmail.com>
2018-06-27 01:05:50 +0000

    updated name of database

--- migrations/20180627005006_foreign_key.js
+++ migrations/20180627005006_foreign_key.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('milestones', function(table) {
-    table.integer('famous_person_id').unsigned();
-    table.foreign('famous_person_id').references('famous_person.id');
+    table.integer('famous_people_id').unsigned();
+    table.foreign('famous_people_id').references('famous_people.id');
     })
     ]);
 };
